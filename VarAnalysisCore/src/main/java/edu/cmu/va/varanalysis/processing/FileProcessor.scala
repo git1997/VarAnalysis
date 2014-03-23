4a1e5c7d6ec1ab7d82bd8c951e44173af2ace7a6
Christian Kaestner <kaestner@cs.cs.cmu.edu>
2014-03-23 14:42:44 -0400

    incorporate SAX parser

--- VarAnalysisCore/src/main/java/edu/cmu/va/varanalysis/processing/FileProcessor.scala
+++ VarAnalysisCore/src/main/java/edu/cmu/va/varanalysis/processing/FileProcessor.scala
@@ -10,6 +10,10 @@ import errormodel.SymExException
 import java.io.File
 import varanalysis.RunFile
 import de.fosd.typechef.parser.html._
+import de.fosd.typechef.featureexpr.FeatureExprFactory
+import de.fosd.typechef.conditional.Opt
+import de.fosd.typechef.featureexpr.FeatureExpr
+import de.fosd.typechef.featureexpr.FeatureExpr
 
 class FileProcessor {
   def process(ifile: IFile, reporter: SymExErrorHandler) {
@@ -24,46 +28,69 @@ class FileProcessor {
       return ;
     }
 
-    parseVarDom(model);
+    parseVarDom(model, reporter);
 
   }
 
-  def parseVarDom(model: DataNode) {
+  def parseVarDom(model: DataNode, reporter: SymExErrorHandler) {
     val tokens = lexDModel(model);
-    
-    assert(tokens!=null)
-    
-    val p = new HTMLSAXParser
 
-    val tagSequence = p.phrase(p.HtmlSequence)(tokens,FeatureExprFactory.True)
+    assert(tokens != null)
 
-    // stage 2: DOM parser
+    val tagSequence = parseSAX(tokens, reporter)
 
-    var domTokens = List[HElementToken]()
-    tagSequence match {
-        case p.Success(r, rest) =>
-            domTokens = r.map(t=>new HElementToken(t))
-            if (!rest.atEnd) println("error: SAXParser not at end: "+rest)
-        case x => println("parsing problem: "+x)
-    }
-
-    println(domTokens.mkString("\n"))
-
-    println("\n\n")
-
-    val p2 = new HTMLDomParser
-
-    val tokenStream = new TokenReader[HElementToken, Null](domTokens, 0, null, new HElementToken(Opt(FeatureExprFactory.True,HText(List()))))
-
-
-    val dom = p2.phrase(p2.Element)(tokenStream,FeatureExprFactory.True)
-
-    println(dom)
-    
+    //    // stage 2: DOM parser
+    //
+    //    var domTokens = List[HElementToken]()
+    //    tagSequence match {
+    //        case p.Success(r, rest) =>
+    //            domTokens = r.map(t=>new HElementToken(t))
+    //            if (!rest.atEnd) println("error: SAXParser not at end: "+rest)
+    //        case x => println("parsing problem: "+x)
+    //    }
+    //
+    //    println(domTokens.mkString("\n"))
+    //
+    //    println("\n\n")
+    //
+    //    val p2 = new HTMLDomParser
+    //
+    //    val tokenStream = new TokenReader[HElementToken, Null](domTokens, 0, null, new HElementToken(Opt(FeatureExprFactory.True,HText(List()))))
+    //
+    //
+    //    val dom = p2.phrase(p2.Element)(tokenStream,FeatureExprFactory.True)
+    //
+    //    println(dom)
 
   }
 
   def lexDModel(model: DataNode): TokenReader[CharacterToken, Object] = new DModelLexer().lex(model);
+
+  def parseSAX(tokens: TokenReader[CharacterToken, Object], reporter: SymExErrorHandler): List[HElementToken] = {
+    val p = new HTMLSAXParser
+    val parseResult = p.phrase(p.HtmlSequence)(tokens, FeatureExprFactory.True)
+
+    def getParseResult(parseResult: p.MultiParseResult[List[Opt[HElement]]], ctx: FeatureExpr): List[Opt[HElement]] = parseResult match {
+      case p.Success(r, rest) =>
+        if (!rest.atEnd)
+          reporter.error(
+            new SymExException("error: SAXParser not at end: " + rest,
+              new File(rest.first.getPosition.getFile),
+              rest.first.getPosition.getColumn))
+        r.map(_.and(ctx))
+      case p.NoSuccess(msg, rest, _) =>
+        reporter.error(
+          new SymExException(msg,
+            new File(rest.first.getPosition.getFile),
+            rest.first.getPosition.getColumn))
+        Nil
+      case p.SplittedParseResult(f, a, b) => getParseResult(a, ctx and f) ++ getParseResult(b, ctx andNot f)
+    }
+
+    val helemlist = getParseResult(parseResult, FeatureExprFactory.True)
+
+    helemlist.map(t => new HElementToken(t))
+  }
 
   def executeSymbolically(reporter: SymExErrorHandler, file: File): DataNode =
     new RunFile(file, new File(".")).run(reporter);
--- VarAnalysisCore/src/test/java/edu/cmu/va/varanalysis/processing/FileProcessorTest.java
+++ VarAnalysisCore/src/test/java/edu/cmu/va/varanalysis/processing/FileProcessorTest.java
@@ -9,6 +9,7 @@ import java.io.File;
 
 import org.junit.Test;
 
+import scala.collection.immutable.List;
 import util.FileIO;
 import varanalysis.RunFile;
 import datamodel.nodes.DataModelVisitor;
@@ -16,6 +17,7 @@ import datamodel.nodes.DataNode;
 import datamodel.nodes.LiteralNode;
 import de.fosd.typechef.parser.TokenReader;
 import de.fosd.typechef.parser.common.CharacterToken;
+import de.fosd.typechef.parser.html.HElementToken;
 import errormodel.SymExErrorHandler;
 import errormodel.SymExException;
 
@@ -26,27 +28,7 @@ public class FileProcessorTest {
 
 	private DataNode symExFile() {
 		DataNode model = new FileProcessor().executeSymbolically(
-				new SymExErrorHandler() {
-
-					@Override
-					public void warning(SymExException exception) {
-						fail("warning occurred" + exception);
-						exception.printStackTrace();
-					}
-
-					@Override
-					public void fatalError(SymExException exception) {
-						fail("fatal error occurred" + exception);
-						exception.printStackTrace();
-					}
-
-					@Override
-					public void error(SymExException exception) {
-						fail("error occurred" + exception);
-						exception.printStackTrace();
-
-					}
-				}, FILE);
+				failOnErrorReporter, FILE);
 		assertNotNull(model);
 		return model;
 	}
@@ -94,6 +76,34 @@ public class FileProcessorTest {
 
 	}
 
+	private final FileProcessor p=new FileProcessor();
+	private SymExErrorHandler failOnErrorReporter=new SymExErrorHandler() {
+
+		@Override
+		public void warning(SymExException exception) {
+			fail("warning occurred" + exception);
+			exception.printStackTrace();
+		}
+
+		@Override
+		public void fatalError(SymExException exception) {
+			fail("fatal error occurred" + exception);
+			exception.printStackTrace();
+		}
+
+		@Override
+		public void error(SymExException exception) {
+			fail("error occurred" + exception);
+			exception.printStackTrace();
+
+		}
+	};
+	
+	@Test
+	public void testSAX() {
+		List<HElementToken> result = p.parseSAX(p.lexDModel(symExFile()), failOnErrorReporter);
+		System.out.println(result);
+	}
 
 	// TODO write test for includes
 }
