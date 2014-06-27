2d9c0b3d701617bcfa3c486d0a696417a7bcd29c
Hung <hungnv@iastate.edu>
2014-06-27 16:52:43 -0500

    Create GenericTreeViewer for reuse

--- SymexUI/src/main/java/edu/iastate/symex/ui/views/DataModelView.java
+++ SymexUI/src/main/java/edu/iastate/symex/ui/views/DataModelView.java
@@ -1,22 +1,12 @@
 package edu.iastate.symex.ui.views;
 
 import java.io.File;
-import java.util.ArrayList;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.jface.viewers.ColumnLabelProvider;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.jface.viewers.TreeViewerColumn;
-import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -26,25 +16,11 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
-import edu.iastate.symex.position.Range;
-import edu.iastate.symex.position.Position;
-import edu.iastate.symex.position.PositionRange;
 import edu.iastate.symex.run.RunSymexForFile;
 import edu.iastate.symex.ui.UIHelper;
-import edu.iastate.symex.constraints.AtomicConstraint;
-import edu.iastate.symex.datamodel.nodes.ConcatNode;
-import edu.iastate.symex.datamodel.nodes.DataNode;
-import edu.iastate.symex.datamodel.nodes.DataNodeFactory;
-import edu.iastate.symex.datamodel.nodes.LiteralNode;
-import edu.iastate.symex.datamodel.nodes.RepeatNode;
-import edu.iastate.symex.datamodel.nodes.SelectNode;
-import edu.iastate.symex.datamodel.nodes.SymbolicNode;
 import edu.iastate.symex.datamodel.DataModel;
-import edu.iastate.symex.datamodel.DataModelVisitor;
 import edu.iastate.symex.datamodel.WriteDataModelToIfDefs;
 
 /**
@@ -111,68 +87,7 @@ public class DataModelView extends ViewPart {
 		TabFolder tabFolder = new TabFolder(parent, SWT.BORDER);
 		tabFolder.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2,	1));
 
-		dataModelTreeViewer = new TreeViewer(tabFolder, SWT.BORDER | SWT.FULL_SELECTION);
-		dataModelTreeViewer.setContentProvider(new DataModelContentProvider());
-		dataModelTreeViewer.setInput(null);
-		dataModelTreeViewer.getTree().setHeaderVisible(true);
-		dataModelTreeViewer.getTree().setLinesVisible(true);
-		//dataModelTreeViewer.getTree().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-
-		TreeViewerColumn column = new TreeViewerColumn(dataModelTreeViewer, SWT.NONE);
-		column.getColumn().setWidth(300);
-		column.getColumn().setText("Tree");
-		column.setLabelProvider(new ColumnLabelProvider() {
-			
-			public String getText(Object element) {
-				return getTypeOfDataNode((DataNode) element);
-			}
-			
-			public Image getImage(Object element) {
-				return getIconForDataNode((DataNode) element);
-			}
-		});
-
-		column = new TreeViewerColumn(dataModelTreeViewer, SWT.NONE);
-		column.getColumn().setWidth(300);
-		column.getColumn().setText("Text");
-		column.setLabelProvider(new ColumnLabelProvider() {
-
-			public String getText(Object element) {
-				return getTextOfDataNode((DataNode) element);
-			}
-		});
-		
-		column = new TreeViewerColumn(dataModelTreeViewer, SWT.NONE);
-		column.getColumn().setWidth(200);
-		column.getColumn().setText("File");
-		column.setLabelProvider(new ColumnLabelProvider() {
-
-			public String getText(Object element) {
-				return UIHelper.standardizeFilePath(getFilePathOfDataNode((DataNode) element));
-			}
-		});
-		
-		column = new TreeViewerColumn(dataModelTreeViewer, SWT.NONE);
-		column.getColumn().setWidth(100);
-		column.getColumn().setText("Line");
-		column.setLabelProvider(new ColumnLabelProvider() {
-
-			public String getText(Object element) {
-				int line = getLineInFileOfDataNode((DataNode) element);
-				return (line >= 0 ? String.valueOf(line) : "");
-			}
-		});
-		
-		column = new TreeViewerColumn(dataModelTreeViewer, SWT.NONE);
-		column.getColumn().setWidth(100);
-		column.getColumn().setText("Offset");
-		column.setLabelProvider(new ColumnLabelProvider() {
-
-			public String getText(Object element) {
-				int offset = getOffsetInFileOfDataNode((DataNode) element);
-				return (offset >= 0 ? String.valueOf(offset) : "");
-			}
-		});
+		dataModelTreeViewer = new DataModelTreeViewer(tabFolder, SWT.BORDER | SWT.FULL_SELECTION);
 		
 		TabItem tabItem1 = new TabItem(tabFolder, SWT.NONE);
 		tabItem1.setText("Tree");
@@ -207,38 +122,7 @@ public class DataModelView extends ViewPart {
 				runSymexButtonClicked();
 			}
     	});
-	    
-	    dataModelTreeViewer.addSelectionChangedListener(new ISelectionChangedListener() {
-
-			@Override
-			public void selectionChanged(SelectionChangedEvent event) {
-				Object selectedObject = ((IStructuredSelection) event.getSelection()).getFirstElement(); 
-				if (selectedObject != null)
-					dataNodeSelected((DataNode) selectedObject);
-			}
-	    });
 	}
-	
-	/**
-	 * Run Symex and show results 
-	 */
-	private void runSymexAndShowResults(File file, File project) {
-		DataModel dataModel = new RunSymexForFile(file).execute();
-		
-		filePathLabel.setText(file.getAbsolutePath());
-		dataModelTreeViewer.setInput(dataModel);
-		dataModelTreeViewer.expandToLevel(2);
-		dataModelStyledText.setText(WriteDataModelToIfDefs.convert(dataModel));
-	}
-	
-	/**
-	 * Clear results
-	 */
-//	private void clearResults() {
-//		filePathLabel.setText("");
-//		dataModelTreeViewer.setInput(null);
-//		dataModelStyledText.setText("");
-//	}
 	
 	/**
 	 * Invoked when the RunSymex button is clicked.
@@ -247,197 +131,20 @@ public class DataModelView extends ViewPart {
 		UIHelper.saveAllEditors();
 		if (UIHelper.getActiveEditor() != null) {
 			IFile file = UIHelper.getActiveEditorFile();
-			IProject iProject = file.getProject();
-			runSymexAndShowResults(UIHelper.iFileToFile(file), UIHelper.iProjectToFile(iProject));
+			runSymexAndShowResults(UIHelper.iFileToFile(file));
 		}
 	}
 	
 	/**
-	 * Invoked when a DataNode is selected
+	 * Run Symex and show results 
 	 */
-	private void dataNodeSelected(DataNode dataNode) {
-		Range range = getRangeOfDataNode(dataNode).getRanges().get(0);
-		UIHelper.selectAndReveal(range.getFile(), range.getOffset(), range.getLength());
-		dataModelTreeViewer.getControl().setFocus();
-	}
-	
-	/**
-	 * SelectChildNode represents a branch of a SelectNode.
-	 */
-	private class SelectChildNode extends DataNode {
+	private void runSymexAndShowResults(File file) {
+		DataModel dataModel = new RunSymexForFile(file).execute();
 		
-		private DataNode dataNode;
-		private boolean isTrueBranch;
-		
-		public SelectChildNode(DataNode dataNode, boolean isTrueBranch) {
-			this.dataNode = dataNode;
-			this.isTrueBranch = isTrueBranch;
-		}
-		
-		public DataNode getDataNode() {
-			return dataNode;
-		}
-		
-		public boolean isTrueBranch() {
-			return isTrueBranch;
-		}
-
-		@Override
-		public void accept(DataModelVisitor dataModelVisitor) {
-			// TODO Auto-generated method stub
-		}
-
-	}
-
-	/**
-	 * Content provider for the Data Model tree.
-	 */
-	private class DataModelContentProvider implements ITreeContentProvider {
-		
-		@Override
-		public void dispose() {
-		}
-
-		@Override
-		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		}
-
-		@Override
-		public Object getParent(Object element) {
-			return null;
-		}
-
-		@Override
-		public Object[] getElements(Object inputElement) {
-			DataModel dataModel = (DataModel) inputElement;
-			DataNode outputDataNode = dataModel.getRoot();
-			if (outputDataNode == null)
-				outputDataNode = DataNodeFactory.createSymbolicNode();
-			return new Object[]{outputDataNode};
-		}
-
-		@Override
-		public Object[] getChildren(Object parentNode) {
-			ArrayList<DataNode> dataNodeList = new ArrayList<DataNode>();
-			
-			if (parentNode instanceof ConcatNode) {
-				dataNodeList.addAll(((ConcatNode) parentNode).getChildNodes());
-			}
-			
-			else if (parentNode instanceof SelectNode) {
-				SelectNode selectNode = (SelectNode) parentNode;
-				if (selectNode.getNodeInTrueBranch() != null)
-					dataNodeList.add(new SelectChildNode(selectNode.getNodeInTrueBranch(), true));
-				if (selectNode.getNodeInFalseBranch() != null)
-					dataNodeList.add(new SelectChildNode(selectNode.getNodeInFalseBranch(), false));
-			}
-			
-			else if (parentNode instanceof SelectChildNode) {
-				dataNodeList.add(((SelectChildNode) parentNode).getDataNode());
-			}
-			
-			else if (parentNode instanceof RepeatNode) {
-				dataNodeList.add(((RepeatNode) parentNode).getChildNode());
-			}
-			
-			else if (parentNode instanceof SymbolicNode) {
-				if (((SymbolicNode) parentNode).getParentNode() != null)
-					dataNodeList.add(((SymbolicNode) parentNode).getParentNode());
-			}
-			
-			else {
-				// LiteralNode has no children.
-			}
-			
-			return dataNodeList.toArray(new DataNode[]{});
-		}
-
-		@Override
-		public boolean hasChildren(Object element) {
-			return (getChildren(element).length > 0);
-		}
-		
-	}
-	
-	/*
-	 * Label provider for Data Nodes
-	 */
-	
-	private String getTypeOfDataNode(DataNode dataNode) {
-		if (dataNode instanceof SelectChildNode)
-			return (((SelectChildNode) dataNode).isTrueBranch() ? "True" : "False");
-		else
-			return dataNode.getClass().getSimpleName().replace("Node", "");
-	}
-	
-	private Image getIconForDataNode(DataNode dataNode) {
-		// http://shinych.blogspot.com/2007/05/eclipse-shared-images.html
-		String imageID;
-		
-		if (dataNode instanceof ConcatNode)
-			imageID = ISharedImages.IMG_OBJ_FOLDER;
-		
-		else if (dataNode instanceof SelectNode)
-			imageID = ISharedImages.IMG_TOOL_CUT;
-		
-		else if (dataNode instanceof SelectChildNode)
-			imageID = ISharedImages.IMG_TOOL_FORWARD;
-		
-		else if (dataNode instanceof RepeatNode)
-			imageID = ISharedImages.IMG_TOOL_REDO;
-		
-		else if (dataNode instanceof SymbolicNode)
-			imageID = ISharedImages.IMG_OBJS_WARN_TSK;
-		
-		else
-			imageID = ISharedImages.IMG_OBJ_FILE;
-		
-		return PlatformUI.getWorkbench().getSharedImages().getImage(imageID);
-	}
-	
-	private String getTextOfDataNode(DataNode dataNode) {
-		if (dataNode instanceof SelectNode)
-			return (((SelectNode) dataNode).getConstraint() != null ? ((AtomicConstraint) ((SelectNode) dataNode).getConstraint()).getConditionString().getStringValue() : "");
-		
-		else if (dataNode instanceof SymbolicNode)
-			return (((SymbolicNode) dataNode).getPhpNode() != null ? ((SymbolicNode) dataNode).getPhpNode().getSourceCode() : "");
-		
-		else if (dataNode instanceof LiteralNode)
-			return UIHelper.standardizeText(((LiteralNode) dataNode).getStringValue());
-		
-		else
-			return "";
-	}
-	
-	private String getFilePathOfDataNode(DataNode dataNode) {
-		File file = getPositionOfDataNode(dataNode).getFile();
-		return (file != null ? file.getPath() : "");
-	}
-	
-	private int getLineInFileOfDataNode(DataNode dataNode) {
-		return getPositionOfDataNode(dataNode).getLine();
-	}
-	
-	private int getOffsetInFileOfDataNode(DataNode dataNode) {
-		return getPositionOfDataNode(dataNode).getOffset();
-	}
-	
-	private PositionRange getRangeOfDataNode(DataNode dataNode) {
-		if (dataNode instanceof SelectNode)
-			return (((SelectNode) dataNode).getConstraint() != null ? ((AtomicConstraint)((SelectNode) dataNode).getConstraint()).getConditionString().getPositionRange() : PositionRange.UNDEFINED);
-		
-		else if (dataNode instanceof SymbolicNode)
-			return (((SymbolicNode) dataNode).getPhpNode() != null ? ((SymbolicNode) dataNode).getPhpNode().getRange() : PositionRange.UNDEFINED);
-		
-		else if (dataNode instanceof LiteralNode)
-			return ((LiteralNode) dataNode).getPositionRange();
-		
-		else
-			return PositionRange.UNDEFINED;
-	}
-	
-	private Position getPositionOfDataNode(DataNode dataNode) {
-		return getRangeOfDataNode(dataNode).getStartPosition();
+		filePathLabel.setText(file.getAbsolutePath());
+		dataModelTreeViewer.setInput(dataModel);
+		dataModelTreeViewer.expandToLevel(2);
+		dataModelStyledText.setText(WriteDataModelToIfDefs.convert(dataModel));
 	}
 	
 }
---
+++ SymexUI/src/main/java/edu/iastate/symex/ui/views/GenericTreeViewer.java
@@ -0,0 +1,231 @@
+package edu.iastate.symex.ui.views;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.TreeViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+
+import edu.iastate.symex.position.PositionRange;
+import edu.iastate.symex.position.Range;
+import edu.iastate.symex.ui.UIHelper;
+
+/**
+ * 
+ * @author HUNG
+ *
+ */
+public abstract class GenericTreeViewer extends TreeViewer {
+	
+	public GenericTreeViewer(Composite parent, int style) {
+		super(parent, style);
+		
+		/*
+		 * Layout
+		 */
+		getTree().setHeaderVisible(true);
+		getTree().setLinesVisible(true);
+		//getTree().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		
+		/*
+		 * Event handling
+		 */
+		addSelectionChangedListener(new ISelectionChangedListener() {
+
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				Object selectedElement = ((IStructuredSelection) event.getSelection()).getFirstElement(); 
+				if (selectedElement != null)
+					treeNodeSelected(selectedElement);
+			}
+	    });
+		
+		/*
+		 * Content provider
+		 */
+		setContentProvider(new MyContentProvider(this));
+		setInput(null);
+		
+		/*
+		 * Label provider
+		 */
+		TreeViewerColumn column = new TreeViewerColumn(this, SWT.NONE);
+		column.getColumn().setWidth(300);
+		column.getColumn().setText("Tree");
+		column.setLabelProvider(new ColumnLabelProvider() {
+			
+			public String getText(Object element) {
+				return getTreeNodeLabel(element);
+			}
+			
+			public Image getImage(Object element) {
+				return getTreeNodeIcon(element);
+			}
+		});
+		
+		column = new TreeViewerColumn(this, SWT.NONE);
+		column.getColumn().setWidth(300);
+		column.getColumn().setText("Text");
+		column.setLabelProvider(new ColumnLabelProvider() {
+
+			public String getText(Object element) {
+				return getTreeNodeDescription(element);
+			}
+		});
+		
+		column = new TreeViewerColumn(this, SWT.NONE);
+		column.getColumn().setWidth(200);
+		column.getColumn().setText("File");
+		column.setLabelProvider(new ColumnLabelProvider() {
+
+			public String getText(Object element) {
+				return getTreeNodeFilePath(element);
+			}
+		});
+		
+		column = new TreeViewerColumn(this, SWT.NONE);
+		column.getColumn().setWidth(100);
+		column.getColumn().setText("Line");
+		column.setLabelProvider(new ColumnLabelProvider() {
+
+			public String getText(Object element) {
+				return getTreeNodeLineRange(element);
+			}
+		});
+		
+		column = new TreeViewerColumn(this, SWT.NONE);
+		column.getColumn().setWidth(100);
+		column.getColumn().setText("Offset");
+		column.setLabelProvider(new ColumnLabelProvider() {
+
+			public String getText(Object element) {
+				return getTreeNodeOffsetRange(element);
+			}
+		});
+	}
+	
+	/*
+	 * Event handling
+	 */
+	
+	public void treeNodeSelected(Object element) {
+		Range range = getTreeNodePositionFirstRange(element);
+		if (range != null) {
+			UIHelper.selectAndReveal(range.getFile(), range.getOffset(), range.getLength());
+			getControl().setFocus();
+		}
+	}
+	
+	/*
+	 * Content provider
+	 */
+	
+	private class MyContentProvider implements ITreeContentProvider {
+		
+		private GenericTreeViewer treeViewer;
+		//private HashMap<Object, Object> childToParentMap = new HashMap<Object, Object>();
+		
+		public MyContentProvider(GenericTreeViewer treeViewer) {
+			this.treeViewer = treeViewer;
+		}
+
+		@Override
+		public void dispose() {
+		}
+
+		@Override
+		public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
+		}
+
+		@Override
+		public Object[] getChildren(Object arg0) {
+			Object[] children = treeViewer.getChildren(arg0);
+			//for (Object child : children)
+			//	childToParentMap.put(arg0, child);
+			return children;
+		}
+
+		@Override
+		public Object[] getElements(Object arg0) {
+			return treeViewer.getRootNodes(arg0);
+		}
+
+		@Override
+		public Object getParent(Object arg0) {
+			return null; //childToParentMap.get(arg0);
+		}
+
+		@Override
+		public boolean hasChildren(Object arg0) {
+			return getChildren(arg0).length > 0;
+		}
+		
+	}
+	
+	public abstract Object[] getRootNodes(Object input);
+	
+	public abstract Object[] getChildren(Object element);
+	
+	/*
+	 * Label provider
+	 */
+	
+	public abstract String getTreeNodeLabel(Object element);
+	
+	public abstract Image getTreeNodeIcon(Object element);
+	
+	public abstract String getTreeNodeDescription(Object element);
+	
+	public String getTreeNodeFilePath(Object element) {
+		Range range = getTreeNodePositionFirstRange(element);
+		return range == null ? "" : UIHelper.standardizeFilePath(range.getFile().getAbsolutePath());
+	}
+	
+	public String getTreeNodeLineRange(Object element) {
+		Range range = getTreeNodePositionFirstRange(element);
+		return range == null ? "" : (String.valueOf(range.getStartPosition().getLine()) + "-" + String.valueOf(range.getEndPosition().getLine()));
+	}
+	
+	public String getTreeNodeOffsetRange(Object element) {
+		Range range = getTreeNodePositionFirstRange(element);
+		return range == null ? "" : (String.valueOf(range.getOffset()) + "-" + String.valueOf(range.getOffset() + range.getLength() - 1));
+	}
+	
+	/*
+	 * Other methods
+	 */
+	
+	public abstract PositionRange getTreeNodePositionRange(Object element);
+	
+	public Range getTreeNodePositionFirstRange(Object element) {
+		ArrayList<Range> ranges = getTreeNodePositionRange(element).getRanges();
+		return ranges.isEmpty() ? null : ranges.get(0);
+	}
+	
+	/**
+	 * This class is used to wrap around the root of a tree.
+	 */
+	public static class TreeInput {
+		
+		private Object root;
+		
+		public TreeInput(Object root) {
+			this.root = root;
+		}
+		
+		public Object getRoot() {
+			return root;
+		}
+		
+	}
+	
+}
