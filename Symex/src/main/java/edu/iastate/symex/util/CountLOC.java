90af2fe06162fa725a13a789285f208764a63ef5
Hung <hungnv@iastate.edu>
2015-02-23 12:07:33 -0600

    Added CountLOC

---
+++ Symex/src/main/java/edu/iastate/symex/util/CountLOC.java
@@ -0,0 +1,29 @@
+package edu.iastate.symex.util;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author HUNG
+ *
+ */
+public class CountLOC {
+	
+	public static String projectName	= "webchess-1.0.0";
+	public static String projectFolder	= "/Work/Data/Web Projects/Server Code/" + projectName;
+
+	public static void main(String[] args) {
+		ArrayList<String> allFiles = FileIO.getAllFilesInFolderByExtensions(projectFolder, new String[]{".php", ".html", ".js"});
+
+		int totalLOC = 0;
+		for (String file : allFiles) {
+			totalLOC += FileIO.getLinesOfCodeInFile(new File(file));
+		}
+		
+		System.out.println("Project folder:  " + projectFolder);
+		System.out.println("Number of files: " + allFiles.size());
+		System.out.println("Total LOC:       " + totalLOC);
+	}
+	
+}
