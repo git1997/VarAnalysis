5dc3f651a2781cfd49a5c110c42292ebb723c555
Hung <hungnv@iastate.edu>
2015-03-05 18:18:52 -0600

    Revised BranchEnv

--- Symex/src/main/java/edu/iastate/symex/core/BranchEnv.java
+++ Symex/src/main/java/edu/iastate/symex/core/BranchEnv.java
@@ -1,6 +1,11 @@
 package edu.iastate.symex.core;
 
 import edu.iastate.symex.constraints.Constraint;
+import edu.iastate.symex.constraints.EqualConstraint;
+import edu.iastate.symex.datamodel.nodes.DataNode;
+import edu.iastate.symex.php.nodes.ExpressionNode;
+import edu.iastate.symex.php.nodes.ScalarNode;
+import edu.iastate.symex.php.nodes.VariableBaseNode;
 
 /**
  * 
@@ -17,6 +22,27 @@ public class BranchEnv extends Env {
 	public BranchEnv(Env outerScopeEnv, Constraint constraint) {
 		super(outerScopeEnv);
 		this.constraint = constraint;
+		
+		// Handle constraints with the form "$x == some_value"
+		handleEqualConstraint();
+	}
+	
+	/**
+	 * Handle constraints with the form "$x == some_value".
+	 * In that case, assign $x with some_value in the branchEnv.
+	 */
+	private void handleEqualConstraint() {
+		if (constraint instanceof EqualConstraint) {
+			ExpressionNode left = ((EqualConstraint) constraint).getLeftExpression();
+			ExpressionNode right = ((EqualConstraint) constraint).getRightExpression();
+			if (left instanceof VariableBaseNode && right instanceof ScalarNode) {
+				// TODO This action causes the expressions to be re-evaluated. Haven't considered its side effects yet.
+				PhpVariable phpVariable = ((VariableBaseNode) left).createVariablePossiblyWithNull(this);
+				DataNode value = right.execute(this);
+				if (phpVariable != null)
+					this.writeVariable(phpVariable, value);
+			}
+		}
 	}
 	
 	public Constraint getConstraint() {
