9ca046e1b8e48bea030ea538224bce43bc03b6e5
Hung <hungnv@iastate.edu>
2015-03-06 20:59:26 -0600

    Updated test for loop

--- Symex/src/test/resources/input/testLoop.php
+++ Symex/src/test/resources/input/testLoop.php
@@ -2,12 +2,39 @@
 
 $x = "Hey ";
 
-
 for ($a = 0; $a <= 10; $a++) {
     $x = $x . "world";
 }
 
 echo $x;
+
+
+
+while ($a < $b) {
+	$y .= $z;
+	
+	if(y != "1")
+		$y .= "2";
+	else
+		$y .= "3";
+}
+
+echo $y;
+
+
+// From SchoolMate-1.5.4/VisualizeClasses.php
+for($j=0; $j<count($days); $j++) {
+	switch($days[$j]) {
+		case 'M':
+			$monday .= "<b>$class[0]</b><br />";
+			if ($monday != "<td>")
+				$monday .= "<br /><br />";
+			else
+				$monday .= "<br />";
+			break;
+	}
+}
+echo $monday;
 
 
 ?>
